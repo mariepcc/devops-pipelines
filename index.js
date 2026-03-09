9cc7ec7aa13d8c2f4bdc7a1b3e896ffc0ba30788
mariepcc <maria.pacocha358@gmail.com>
2026-03-09 11:38:08 +0100

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello Devops!");
+console.lof("I am learning CI/CD using Github Actions");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+setTimeout(() => {
+  console.log("Waiting 3 seconsds...");
+}, 3000);
+console.log("Tests completed!");
